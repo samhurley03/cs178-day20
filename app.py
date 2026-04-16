96461d4b8a66294615dbdaca97e6c24ccd46e810
Sam <sam.hurley@drake.edu>
2026-04-16 09:36:26 -0500

    golf

--- app.py
+++ app.py
@@ -1,46 +1,16 @@
 from flask import Flask
 import os
 import socket
-import random
 
 app = Flask(__name__)
 
-# List of programming jokes
-jokes = [
-    "Why do programmers prefer dark mode? Because light attracts bugs.",
-    "Why did the developer go broke? Because he used up all his cache.",
-    "There are 10 types of people in the world: those who understand binary and those who dont.",
-    "I would tell you a UDP joke, but you might not get it."
-]
-
 @app.route("/")
 def hello():
-    hostname = socket.gethostname()
-    name = os.getenv("NAME", "CS178 Student")
-
-    html = f"""
-    <html>
-        <head>
-            <title>Sam's Friendly Flask App</title>
-        </head>
-        <body style="font-family: Arial; text-align: center; padding-top: 40px;">
-            <h1>👋 Welcome, {name}!</h1>
-            <h3>This app is running inside Docker 🚀</h3>
-            <p><b>Hostname:</b> {hostname}</p>
-
-            <h2>💡 Random Programming Joke</h2>
-            <p><i>{random.choice(jokes)}</i></p>
-
-            <h2>🐳 Docker + Flask = Powerful Combo</h2>
-            <img src="https://miro.medium.com/max/1400/1*4X5n6WfR9YH9xZlKXzJpHg.png" width="400">
-
-            <p style="margin-top:40px;">Built with Flask & Docker</p>
-        </body>
-    </html>
-    """
-
-    return html
-
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<h2>Here is an interesting golf course picture for you: </h2>" \
+           '<img src="https://www.top100golfcourses.com/_next/image?url=https:%2F%2Fcdn.sanity.io%2Fimages%2F03mhssoh%2Fproduction%2F282ce428f9af676a8104f24dfa4219969d356a5f-2400x1404.jpg&w=1920&q=75">'
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
