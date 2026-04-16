683e2976108f5be23f308c3f1e3f8384aabe055e
Sam <sam.hurley@drake.edu>
2026-04-16 09:26:07 -0500

    comma

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 jokes = [
     "Why do programmers prefer dark mode? Because light attracts bugs.",
     "Why did the developer go broke? Because he used up all his cache.",
-    "There are 10 types of people in the world: those who understand binary and those who don’t.",
+    "There are 10 types of people in the world: those who understand binary and those who dont.",
     "I would tell you a UDP joke, but you might not get it."
 ]
 
